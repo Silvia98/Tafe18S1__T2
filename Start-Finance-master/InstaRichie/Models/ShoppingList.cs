f76d676b5294a32da2d8c169e8c6ba35cd793de8
ThiTran <thi.TRAN93@student.tafesa.edu.au>
2018-06-20 14:57:38 +0930

    add ShoppingList

---
+++ Start-Finance-master/InstaRichie/Models/ShoppingList.cs
@@ -0,0 +1,27 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class ShoppingList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ShoppingItemID { get; set; }
+
+        [NotNull]
+        public string ShopName { get; set; }
+
+        [NotNull]
+        public string NameOfItem { get; set; }
+
+        [NotNull]
+        public string ShoppingDate { get; set; }
+
+        [NotNull]
+        public string PriceQuoted { get; set; }
+    }
+}
---
+++ Start-Finance-master/InstaRichie/Views/ShoppingListPage.xaml.cs
@@ -0,0 +1,136 @@
+﻿using SQLite.Net;
+using StartFinance.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace StartFinance.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShoppingListPage : Page
+    {
+        SQLiteConnection conn; // adding an SQLite connection
+        string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
+        public ShoppingListPage()
+        {
+            this.InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+            /// Initializing a database
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+
+            // Creating table
+            Results();
+        }
+
+        public void Results()
+        {
+            // Creating table
+            conn.CreateTable<ShoppingList>();
+            var query = conn.Table<ShoppingList>();
+            TransactionList.ItemsSource = query.ToList();
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+
+        {
+            try
+            {
+
+                if (txtItemName.Text.ToString() == "" || txtQuotedPrice.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Item Name Not Entered...");
+                    await dialog.ShowAsync();
+                }
+
+                else
+                {   // Inserts the data
+                    conn.Insert(new ShoppingList()
+                    {
+                        NameOfItem = txtItemName.Text,
+                        ShopName = txtShopName.Text,
+                        ShoppingDate = txtShoppingDate.Text,
+                        PriceQuoted = txtQuotedPrice.Text,
+                    });
+                    Results();
+                }
+
+            }
+            catch (Exception ex)
+            {   // Exception to display when amount is invalid or not numbers
+                if (ex is FormatException)
+                {
+                    MessageDialog dialog = new MessageDialog("You forgot to enter the Amount or entered an invalid data", "Oops..!");
+                    await dialog.ShowAsync();
+                }   // Exception handling when SQLite contraints are violated
+                else if (ex is SQLiteException)
+                {
+                    MessageDialog dialog = new MessageDialog("ItemID Already Exists..");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    /// no idea
+                }
+
+            }
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Results();
+        }
+
+        private async void DeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog ShowConf = new MessageDialog("Deleting this Account will delete all transactions of this account", "Important");
+            ShowConf.Commands.Add(new UICommand("Yes, Delete")
+            {
+                Id = 0
+            });
+            ShowConf.Commands.Add(new UICommand("Cancel")
+            {
+                Id = 1
+            });
+            ShowConf.DefaultCommandIndex = 0;
+            ShowConf.CancelCommandIndex = 1;
+
+            var result = await ShowConf.ShowAsync();
+            if ((int)result.Id == 0)
+            {
+                // checks if data is null else inserts
+                try
+                {
+                    int ShoppingItemID = ((ShoppingList)TransactionList.SelectedItem).ShoppingItemID;
+                    string ShopName = ((ShoppingList)TransactionList.SelectedItem).ShopName;
+                    var querydel = conn.Query<ShoppingList>("DELETE FROM ShoppingList WHERE ShoppingItemID='" + ShoppingItemID + "' AND ShopName='" + ShopName + "'");
+                    Results();
+                }
+                catch (NullReferenceException)
+                {
+                    MessageDialog ClearDialog = new MessageDialog("Please select the item to Delete", "Oops..!");
+                    await ClearDialog.ShowAsync();
+                }
+            }
+            else
+            {
+                //
+            }
+        }
+    }
+}
